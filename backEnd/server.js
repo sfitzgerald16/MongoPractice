0c04c3b9e0a13b1039f41a88b985378e0129056d
sfitzgerald16 <misfitz616@gmail.com>
2017-07-11 22:24:32 -0700

    added back end

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const parser = require('body-parser');
 const port = process.env.PORT || 5000;
+const app = express();
 
-app.use(bodyParser.json())
+app.use(parser.json());
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+})
 
 app.post('/api/message', function(req, res) {
   console.log(req.body);
